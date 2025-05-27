959bb112e4c3c4c69c05b2cd29bfd7d02a4750cf
anngo-1 <andngo2004@gmail.com>
2025-05-27 00:51:20 -0700

    dev envs

--- nixos/dev-shells/python.nix
+++ nixos/dev-shells/python.nix
@@ -5,17 +5,18 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     python3
     python3Packages.pip
-    python3Packages.venv
     python3Packages.ipython
     python3Packages.jupyter
     python3Packages.requests
     python3Packages.numpy
     python3Packages.pandas
+    python3Packages.virtualenv
   ];
   
   shellHook = ''
     echo "🐍 Python development environment activated"
     export DEV_SHELL_NAME="python"
+    echo "Available: python3, pip, ipython, jupyter, numpy, pandas"
+    echo "Create venv with: python -m venv myenv"
   '';
 }
-
