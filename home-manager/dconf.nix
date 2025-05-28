b08b2a14075e5e43c59e2d3745d7df9557a2db19
anngo-1 <andngo2004@gmail.com>
2025-05-27 23:47:27 -0700

    just use dconf2nix

--- home-manager/dconf.nix
+++ home-manager/dconf.nix
@@ -1,70 +1,179 @@
-# gnome dconf settings exported from your current configuration
-# this file is imported by home.nix
+# Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
+{ lib, ... }:
+
+with lib.hm.gvariant;
+
 {
   dconf.settings = {
-    # gnome shell settings
-    "org/gnome/shell" = {
-      enabled-extensions = ['dash-to-dock@micxgx.gmail.com'];
-      favorite-apps = ['org.gnome.Console.desktop', 'firefox.desktop', 'org.gnome.Nautilus.desktop', 'code.desktop', 'discord.desktop'];
+    "org/gnome/Console" = {
+      last-window-maximised = false;
+      last-window-size = mkTuple [ 732 528 ];
     };
 
-    # Dynamic workspaces setting
-    "org/gnome/mutter" = {
-      dynamic-workspaces = false;
-      num-workspaces = 4;
+    "org/gnome/Geary" = {
+      migrated-config = true;
     };
 
-    # dash to dock settings
-    "org/gnome/shell/extensions/dash-to-dock" = {
-      hot-keys = false;
+    "org/gnome/control-center" = {
+      last-panel = "keyboard";
+      window-state = mkTuple [ 980 640 false ];
     };
 
-    # Custom keybindings
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      custom-keybindings = ['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/'];
+    "org/gnome/desktop/app-folders" = {
+      folder-children = [ "System" "Utilities" "YaST" "Pardus" ];
     };
 
-    # Custom keybinding: term
-    "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" = {
-      name = "term";
-      command = "kgx";
-      binding = "<Super>Return";
+    "org/gnome/desktop/app-folders/folders/Pardus" = {
+      categories = [ "X-Pardus-Apps" ];
+      name = "X-Pardus-Apps.directory";
+      translate = true;
     };
 
-    # window manager keybindings
-    "org/gnome/desktop/wm/keybindings" = {
-      close = ['<Super>q'];
-      switch-applications = ['<Super>Tab', '<Alt>Tab'];
-      switch-applications-backward = ['<Shift><Super>Tab', '<Shift><Alt>Tab'];
-      switch-to-workspace-left = ['<Super>Page_Up', '<Super><Alt>Left', '<Control><Alt>Left'];
-      switch-to-workspace-right = ['<Super>Page_Down', '<Super><Alt>Right', '<Control><Alt>Right'];
-      switch-to-workspace-1 = ['<Super>1'];
-      switch-to-workspace-2 = ['<Super>2'];
-      switch-to-workspace-3 = ['<Super>3'];
-      switch-to-workspace-4 = ['<Super>4'];
+    "org/gnome/desktop/app-folders/folders/System" = {
+      apps = [ "org.gnome.baobab.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.Logs.desktop" "org.gnome.SystemMonitor.desktop" ];
+      name = "X-GNOME-Shell-System.directory";
+      translate = true;
     };
 
-    # interface settings
+    "org/gnome/desktop/app-folders/folders/Utilities" = {
+      apps = [ "org.gnome.Connections.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.font-viewer.desktop" "org.gnome.Loupe.desktop" "org.gnome.seahorse.Application.desktop" ];
+      name = "X-GNOME-Shell-Utilities.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/YaST" = {
+      categories = [ "X-SuSE-YaST" ];
+      name = "suse-yast.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/background" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-l.svg";
+      picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-d.svg";
+      primary-color = "#241f31";
+      secondary-color = "#000000";
+    };
+
+    "org/gnome/desktop/input-sources" = {
+      sources = [ (mkTuple [ "xkb" "us" ]) ];
+      xkb-options = [];
+    };
+
     "org/gnome/desktop/interface" = {
-      gtk-theme = "Adwaita-dark";
-      color-scheme = "prefer-dark";
-      clock-show-weekday = false;
+      accent-color = "red";
       clock-show-seconds = false;
+      clock-show-weekday = false;
+      color-scheme = "prefer-dark";
+      gtk-theme = "Adwaita-dark";
     };
 
-    # mouse settings
+    "org/gnome/desktop/notifications" = {
+      application-children = [ "org-gnome-geary" "org-gnome-software" ];
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-geary" = {
+      application-id = "org.gnome.Geary.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-software" = {
+      application-id = "org.gnome.Software.desktop";
+    };
+
     "org/gnome/desktop/peripherals/mouse" = {
       natural-scroll = true;
     };
 
-    # touchpad settings
     "org/gnome/desktop/peripherals/touchpad" = {
       natural-scroll = false;
       tap-to-click = true;
     };
 
-    # nautilus settings
-    "org/gnome/nautilus/preferences" = {
+    "org/gnome/desktop/screensaver" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-l.svg";
+      primary-color = "#241f31";
+      secondary-color = "#000000";
     };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Super>q" ];
+      switch-applications = [ "<Super>Tab" "<Alt>Tab" ];
+      switch-applications-backward = [ "<Shift><Super>Tab" "<Shift><Alt>Tab" ];
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      switch-to-workspace-left = [ "<Super>Page_Up" "<Super><Alt>Left" "<Control><Alt>Left" ];
+      switch-to-workspace-right = [ "<Super>Page_Down" "<Super><Alt>Right" "<Control><Alt>Right" ];
+    };
+
+    "org/gnome/epiphany" = {
+      ask-for-default = false;
+    };
+
+    "org/gnome/epiphany/state" = {
+      is-maximized = false;
+      window-size = mkTuple [ 1024 768 ];
+    };
+
+    "org/gnome/evolution-data-server" = {
+      migrated = true;
+    };
+
+    "org/gnome/mutter" = {
+      dynamic-workspaces = false;
+    };
+
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "icon-view";
+      migrated-gtk-settings = true;
+      search-filter-time-type = "last_modified";
+    };
+
+    "org/gnome/nautilus/window-state" = {
+      initial-size = mkTuple [ 890 550 ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Super>Return";
+      command = "kgx";
+      name = "term";
+    };
+
+    "org/gnome/shell" = {
+      disabled-extensions = [];
+      enabled-extensions = [ "dash-to-dock@micxgx.gmail.com" ];
+      favorite-apps = [ "org.gnome.Console.desktop" "firefox.desktop" "org.gnome.Nautilus.desktop" "code.desktop" "discord.desktop" ];
+      welcome-dialog-last-shown-version = "48.1";
+    };
+
+    "org/gnome/shell/extensions/dash-to-dock" = {
+      background-opacity = 0.8;
+      dash-max-icon-size = 64;
+      dock-position = "BOTTOM";
+      height-fraction = 0.9;
+      hot-keys = false;
+      intellihide-mode = "FOCUS_APPLICATION_WINDOWS";
+      preferred-monitor = -2;
+      preferred-monitor-by-connector = "eDP-1";
+      pressure-threshold = 0.0;
+    };
+
+    "org/gnome/shell/world-clocks" = {
+      locations = [];
+    };
+
+    "org/gnome/software" = {
+      check-timestamp = mkInt64 1748401446;
+      first-run = false;
+    };
+
   };
 }
